4e2db164a306ce9a265f8214cd4e4cd135ba16d3
Your Name Here <your_email@example.com>
2013-03-20 01:19:35 -0300

    second

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,24 @@
+require "bundler/capistrano"
+
+set :application, "blogx"
+set :scm, :git
+set :repository,  "git://github.com/inforserver2/blogx.git"
 
 
+server "192.168.1.5", :app, :web, :db, primary:true
+ssh_options[:port] = 2222
 
+set :user, "hribeiro"
+set :group, "hribeiro"
+set :deploy_to, "/var/web/unnys.co"
+set :use_sudo, false
 
+set :deploy_via, :copy
+set :copy_strategy, :export
 
-set :application, "blog"
-set :repository,  "set your repository location here"
-
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
-after "deploy:restart", "deploy:cleanup"
+#after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
